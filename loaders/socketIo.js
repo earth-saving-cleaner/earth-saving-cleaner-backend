4a83a2293472cf538ee03cc659291f071ea35dcf
minji <eunoia2040@gmail.com>
2022-02-03 12:42:14 +0900

    chore: add socket connection

--- loaders/socketIo.js
+++ loaders/socketIo.js
@@ -3,10 +3,14 @@ const { Server } = require("socket.io");
 module.exports = (server, app) => {
   const io = new Server(server);
   app.set("io", io);
+  const test = io.of("/test");
 
-  io.on("connection", (socket) => {
+  test.on("connection", (socket) => {
     console.log("a user connected");
-    socket.on("disconnect", () => {
+    socket.emit("test", "test!!!!!");
+
+    socket.on("disconnect", (test) => {
+      console.log(test);
       console.log("user disconnected");
     });
   });
