6041dcadc648d2e1e541f46dacc7301e56c8264b
JK-Yun777 <ramieta16@gmail.com>
2022-02-04 11:17:25 +0900

    Test

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(require("./routes"));
+console.log("TEST");
 
 app.use((req, res, next) => {
   next(createError(404));
