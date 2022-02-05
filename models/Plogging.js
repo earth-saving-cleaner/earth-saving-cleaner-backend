13798684c4111656ca63e4b8ab317b9c49123ad6
minji <eunoia2040@gmail.com>
2022-02-06 03:03:17 +0900

    fix: edit plogging schma

--- models/Plogging.js
+++ models/Plogging.js
@@ -5,18 +5,20 @@ const {
   Types: { ObjectId },
 } = Schema;
 
-const ploggingSchema = new Schema({
-  author: {
-    type: ObjectId,
-    ref: "User",
-    required: [true, "Please provide author id"],
+const ploggingSchema = new Schema(
+  {
+    author: {
+      type: ObjectId,
+      ref: "User",
+      required: [true, "Please provide author id"],
+    },
+    feedId: {
+      type: ObjectId,
+      ref: "Feed",
+      required: [true, "Please provide feed id"],
+    },
   },
-  feedId: {
-    type: ObjectId,
-    ref: "Feed",
-    required: [true, "Please provide feed id"],
-  },
-  timestamps: { createdAt: true },
-});
+  { timestamps: { createdAt: true } },
+);
 
 module.exports = mongoose.model("Plogging", ploggingSchema);
