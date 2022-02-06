c5635f173acc186ad7bd6e84015596ea5ba21cf9
Kyeong1024 <aofldjxm@gmail.com>
2022-02-07 01:18:57 +0900

    fix: edit send name  image to profileImage

--- services/user.js
+++ services/user.js
@@ -4,13 +4,13 @@ const User = require("../models/User");
 
 exports.getUser = async (userId) => {
   const id = mongoose.Types.ObjectId(userId);
-  const { nickname, level, score, totalScore, image } = await User.findOne({ _id: id });
+  const { nickname, level, score, totalScore, profileImage } = await User.findOne({ _id: id });
 
   return {
     nickname,
     level,
     score,
     totalScore,
-    image,
+    profileImage,
   };
 };
