dd0e4515dc3d9c32de5958181a08dac4beb8fe3f
minji <eunoia2040@gmail.com>
2022-02-06 15:51:56 +0900

    feat: add validation for feed list api

--- contorollers/feed.js
+++ contorollers/feed.js
@@ -1,12 +1,20 @@
+const mongoose = require("mongoose");
+const createError = require("http-errors");
+
 const feedService = require("../services/feed");
 const { resultMsg } = require("../constants");
 
 exports.getFeeds = async (req, res, next) => {
   try {
     const { id, limit } = req.query;
+
+    if (id && !mongoose.isValidObjectId(id)) {
+      next(createError(400, "Invalid feed last feed id"));
+    }
+
     const option = {
       lastId: id,
-      limit: limit || 30,
+      limit: validateLimit(limit),
     };
 
     const feeds = await feedService.getFeeds(option);
@@ -20,4 +28,18 @@ exports.getFeeds = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+const validateLimit = (limit) => {
+  const LIMIT = 30;
+
+  if (typeof limit !== "number" || typeof limit !== "string") {
+    return LIMIT;
+  }
+
+  if (limit <= 0) {
+    return LIMIT;
+  }
+
+  return Number(limit);
 };
