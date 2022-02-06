3513584fad4d4f25e90b314a15ec43523c2fa113
Kyeong1024 <aofldjxm@gmail.com>
2022-02-07 01:16:11 +0900

    feat: add user image in user send data

--- services/user.js
+++ services/user.js
@@ -4,12 +4,13 @@ const User = require("../models/User");
 
 exports.getUser = async (userId) => {
   const id = mongoose.Types.ObjectId(userId);
-  const { nickname, level, score, totalScore } = await User.findOne({ _id: id });
+  const { nickname, level, score, totalScore, image } = await User.findOne({ _id: id });
 
   return {
     nickname,
     level,
     score,
     totalScore,
+    image,
   };
 };
