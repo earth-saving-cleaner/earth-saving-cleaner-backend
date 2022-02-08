c3733bc3d0bbd470e26c9d45abd901fe42a6c7a7
Kyeong1024 <aofldjxm@gmail.com>
2022-02-09 04:46:08 +0900

    fix: add coordinates

--- contorollers/location.js
+++ contorollers/location.js
@@ -4,14 +4,15 @@ const { resultMsg } = require("../constants");
 // [37.514035, 127.05151, 37.50188, 127.06708] -> Mock data nw.lat nw.lng se.lat se.lng
 exports.getFeedsLocation = async (req, res, next) => {
   try {
-    const { nwLat, nwLng, seLat, seLng } = req.query;
+    const { coordinates } = req.query;
+    const { nwLng, nwLat, seLng, seLat } = JSON.parse(coordinates);
 
     const polygonCoordinates = [
-      [Number(nwLng), Number(nwLat)],
-      [Number(seLng), Number(nwLat)],
-      [Number(seLng), Number(seLat)],
-      [Number(nwLng), Number(seLat)],
-      [Number(nwLng), Number(nwLat)],
+      [nwLng, nwLat],
+      [seLng, nwLat],
+      [seLng, seLat],
+      [nwLng, seLat],
+      [nwLng, nwLat],
     ];
 
     const feedInfo = await locationService.getFeedsLocation(polygonCoordinates);
