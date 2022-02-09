07eb5c6f29a5c48025f1d2f0bcc88a9aca48a2d0
minji <eunoia2040@gmail.com>
2022-02-09 23:24:27 +0900

    feat: add feed validation rule

--- routes/feed.js
+++ routes/feed.js
@@ -6,7 +6,7 @@ const AWS = require("aws-sdk");
 const multerS3 = require("multer-s3");
 
 const feedController = require("../contorollers/feed");
-const { commentValidationRules, validate } = require("../middlewares/validator");
+const { commentValidationRules, feedValidationRules, validate } = require("../middlewares/validator");
 
 const router = express.Router();
 
@@ -35,8 +35,8 @@ router.post("/img", upload.single("img"), (req, res) => {
 });
 
 router.post("/:id/comment", commentValidationRules(), validate, feedController.createComment);
+router.post("/", feedValidationRules(), validate, feedController.createFeed);
 router.put("/:id/like", feedController.addLikeUser);
-
 router.get("/:id", feedController.getFeed);
 
 module.exports = router;
