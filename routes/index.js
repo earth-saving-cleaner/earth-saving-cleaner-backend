6ae1c7e0a5442b213a3cbc2426179164ee0ca338
minji <eunoia2040@gmail.com>
2022-02-12 07:24:49 +0900

    feat: add router '/signup'

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const { getUser } = require("../contorollers/user");
 const feed = require("./feed");
 const feeds = require("./feeds");
 const user = require("./user");
 const login = require("./login");
 
+const { signup } = require("../contorollers/signup");
 const User = require("../models/User");
 const Commemt = require("../models/Comment");
 const Geo = require("../models/Geo");
@@ -17,5 +17,6 @@ router.use("/user", user);
 router.use("/feeds", feeds);
 router.use("/feed", feed);
 router.use("/login", login);
+router.post("/signup", signup);
 
 module.exports = router;
